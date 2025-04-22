78c5648df8a0ce7d805435d9b8343a9ffe6b779c
Arruti <arruti750@gmail.com>
2025-04-21 21:30:48 -0600

    Edición de un archivo en la terminal con Nano

---
+++ castle.sh
@@ -0,0 +1,18 @@
+#Realizado con Nano 
+#⌃ -> ctrl
+#M -> ESC 
+
+echo "
+Welcome to my castle
+"
+
+echo "
+ /\                /\ 
+/  \              /  \ 
+|  |______________|  |
+|                    |
+|   []    []    []   |
+|                    |
+|         __         |
+|________|  |________|
+"
